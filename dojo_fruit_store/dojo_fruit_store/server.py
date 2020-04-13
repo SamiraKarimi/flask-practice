36e69a372c4620791e428edcadaa4d63bac703e0
SamiraKarimi <samira.ka006@gmail.com>
2020-04-12 21:20:33 -0500

    adding-session-in-project

--- dojo_fruit_store/dojo_fruit_store/server.py
+++ dojo_fruit_store/dojo_fruit_store/server.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,session
 app = Flask(__name__)  
-
+app.secret_key = 'keep it secret, keep it safe'
 @app.route('/')         
 def index():
     return render_template("index.html")
@@ -9,14 +9,24 @@ def index():
 def checkout():
     print(request.form)
     print('*'*60)
+    session['fn'] = request.form['first_name']
+    session['ln'] = request.form['last_name']
+    session['id'] = request.form['student_id']
+    session['str'] = request.form['strawberry']
+    session['ra'] = request.form['raspberry']
+    session['ap'] = request.form['apple']
+
     str=request.form['strawberry']
     ra=request.form['raspberry']
     ap=request.form['apple']
     print(f'the number of fruit: {int(str)+int(ra)+int(ap)}')
-    return render_template("checkout.html",fn=request.form['first_name'],
-    ln=request.form['last_name'],id=request.form['student_id'],  
-    str=request.form['strawberry'],
-    ra=request.form['raspberry'],ap=request.form['apple'])
+    return redirect('/fruits')
+@app.route('/fruits')
+def show_result():
+    return render_template("checkout.html",fn=session['fn'],
+    ln=session['ln'],id=session['id'],  
+    str=session['str'],
+    ra=session['ra'],ap=session['ap'])
 
 
 @app.route('/fruits')         
